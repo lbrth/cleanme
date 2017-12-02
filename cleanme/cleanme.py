3ab0d0cbb03581adc0bef1271dfbd6f35ddcf5e5
boc_d1 <olivierlabarthe6@gmail.com>
2017-12-02 10:35:10 +0100

    argument parser updated

--- cleanme/cleanme.py
+++ cleanme/cleanme.py
@@ -1,66 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 
-
-import os
-import os.path
-import shutil
-import PTN
 import argparse
 import progressbar
 import time
-import sys
 from video import Video
 from managefiles import ManageFiles
 from workers import Worker
-
-
-video = Video()
-data_files = ManageFiles()
-worker = Worker()
-
-
-
-"""
-def manage_data_video(path,path_dest=None):
-
-
-	main function which initiate video metadata analysis, extract metadata, sorting according metadata
-
-
-	global list_video_files
-
-	list_video_files = []
-
-
-	list_files = data_files.list_files(path)
-
-	i = 0
-	while i <= len(data_files.list_files(path)):
-
-		for files in list_files:
-
-			if video.check(path,files):
-
-				list_video_files.append(files)
-
-				if path_dest != None:
-
-					#sorting_video_files(path,files,path_dest)
-					worker.files_sorting()
-
-				else:
-
-					sorting_video_files(path,files)
-
-
-		bar.update(i)
-
-		i += 1
-	print(list_video_files)
-	bar.finish()
-
-"""
 
 
 def main():
@@ -69,11 +15,8 @@ def main():
 	main function to launch the script
 
 	"""
-
-	global bar
-	global path
-	global filename
-	global file_extension
+	data_files = ManageFiles()
+	worker = Worker()
 
 
 	bar = progressbar.ProgressBar(
@@ -82,7 +25,9 @@ def main():
 
 
 	parser = argparse.ArgumentParser(description=" cleanme is a python script to clean a garbage directory, like your download directory")
-	parser.add_argument('-p','--path', action="store",help=" Enter the path of the directory to be cleaned",type=str,default="")
+	parser.add_argument('-p','--path',action="store",help=" Enter the source path of the directory to be cleaned",type=str,required=True)
+	parser.add_argument('-wd','--with_deduplication',action="store_true",help=" Use -wd to activate the deduplication feature with the cleaning job", default=False)
+	parser.add_argument('-od','--only_deduplication',action="store_true",help=" Use -od to activate only the deduplication feature", default=False)
 	parser.add_argument('-d','--destination',action='store',help=" Use -d to specify a path directory to copy cleaned data",type=str,default="")
 
 	args = parser.parse_args()
@@ -91,40 +36,73 @@ def main():
 
 		path = args.path
 
-		if not args.destination:
+		if args.path and not args.with_deduplication and not args.destination and not args.only_deduplication :
 
-			print(" -> Starting script for video files only ...")
+			print("path only")
 
-
-			for i in range(len(data_files.list_files(path))):
-				worker.files_deduplication(path)
-				bar.update(i)
-			bar.finish()
-			print("Nb duplicate : " + str(worker.count_duplicate))
+			print("Cleanme will start to cleaned up your video file directory in the path " + path)
+			time.sleep(2)
 			for i in range(len(data_files.list_files(path))):
 				worker.files_sorting(path)
 				bar.update(i)
 			bar.finish()
-			print("NB video file managed after deduplication " + str(worker.count_video_files))
+			print("Job done. " + str(worker.count_video_files) + " video files managed" )
 
-
-
-
-		elif args.destination:
+		elif args.path and args.destination and not args.with_deduplication:
 
 			path_dest = args.destination
 
-			print(" -> Starting script for vide files with a specific destination")
+			print("Cleanme will start to cleaned up your video file directory in the path " + path + " \n and moved it to " + path_dest)
+			time.sleep(2)
+			for i in range(len(data_files.list_files(path))):
+				worker.files_sorting(path, path_dest)
+				bar.update(i)
+			bar.finish()
+			print("Job done. " + str(worker.count_video_files) + " video files managed" )
+
+		elif args.path and args.with_deduplication and not args.destination:
+			print("Cleame will start to deduplicate your video files and then start the job to cleaned up your video file directory in the path " + path)
+			time.sleep(2)
 			for i in range(len(data_files.list_files(path))):
 				worker.files_deduplication(path)
 				bar.update(i)
 			bar.finish()
-			print("Nb duplicate : " + str(worker.count_duplicate))
+			print("Deduplication job done. " + str(worker.count_duplicate) + " video files deleted during deduplication")
+			time.sleep(2)
+			print("Cleanme will start now to cleaned up your video file directory")
 			for i in range(len(data_files.list_files(path))):
-				worker.files_sorting(path,path_dest)
+				worker.files_sorting(path)
 				bar.update(i)
 			bar.finish()
-			print("NB video file managed after deduplication " + str(worker.count_video_files))
+			print("Job done. " + str(worker.count_video_files) + " video files managed" )
+
+		elif args.path and args.with_deduplication and args.destination:
+			path_dest = args.destination
+			print("Cleame will start to deduplicate your video files and then start the job to cleaned up your video file directory in the path " + path)
+			time.sleep(2)
+			for i in range(len(data_files.list_files(path))):
+				worker.files_deduplication(path)
+				bar.update(i)
+			bar.finish()
+			print("Deduplication job done. " + str(worker.count_duplicate) + " video files deleted during deduplication")
+			time.sleep(2)
+			print("Cleanme will start now to cleaned up your video file directory in the path " + path + " and moved it to " + path_dest)
+			time.sleep(2)
+			for i in range(len(data_files.list_files(path))):
+				worker.files_sorting(path, path_dest)
+				bar.update(i)
+			bar.finish()
+			print("Job done. " + str(worker.count_video_files) + " video files managed" )
+
+		elif args.path and args.only_deduplication and not args.with_deduplication and not args.destination:
+			print("Cleame will start to deduplicate your video files in the path " + path)
+			time.sleep(2)
+			for i in range(len(data_files.list_files(path))):
+				worker.files_deduplication(path)
+				bar.update(i)
+			bar.finish()
+			print("Deduplication job done. " + str(worker.count_duplicate) + " video files deleted during deduplication")
+
 
 
 	except FileNotFoundError as e:
