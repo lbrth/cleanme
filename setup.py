c6ca5a11764790a55c0653145c2e89e1b3192d38
boc_d1 <olivierlabarthe6@gmail.com>
2017-12-04 14:58:47 +0100

    change version in setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
 name = 'cleanme',
 description = 'Python script to clean and deduplicate video files',
 url = 'https://github.com/lbrth/cleanme',
-version = '2.0',
+version = '1.1',
 author = 'Olivier Labarthe',
 author_email = 'olivierlabarthe6@gmail.com',
 packages=find_packages(),
